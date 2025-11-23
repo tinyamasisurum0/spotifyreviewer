a20fb450cee1689d4fa2176d74cef961e8d93875
DeveloCo Team <tinyamasisurum0@gmail.com>
2025-11-23 16:09:18 +0300

    Restore Topster route and listings

--- components/HeaderNav.tsx
+++ components/HeaderNav.tsx
@@ -9,6 +9,7 @@ const navLinks = [
   { href: '/blog', label: 'Blog' },
   { href: '/', label: 'Reviews' },
   { href: '/tier-maker', label: 'Tier Maker' },
+  { href: '/topster', label: 'Topster' },
   { href: '/review-builder', label: 'Review Builder' },
 ];
 
